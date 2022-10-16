6d38d325e86a8be3f01649d6f3d2c394d0a9c4b2
silaev <silaevegor2003@mail.ru>
2022-10-16 20:02:54 +0700

    add all

--- portal/studio/models.py
+++ portal/studio/models.py
@@ -1,9 +1,11 @@
+import uuid
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.urls import reverse
 from django.utils.crypto import get_random_string
 from django.core.validators import FileExtensionValidator
 from django.dispatch import Signal
+from datetime import date
 
 
 # Create your models here.
@@ -19,12 +21,6 @@ class User(AbstractUser):
     role = models.CharField(max_length=200, verbose_name='Роль',
                             choices=(('admin', 'Администратор'), ('user', 'Пользователь')), default='user')
     USERNAME_FIELD = 'username'
-
-    def delete(self, *args, **kwargs):
-        for order_bb in self.order_bb_set.all():
-            order_bb.delete()
-        super().delete(*args, **kwargs)
-
     def __str__(self):
         return str(self.name) + ' ' + str(self.surname)
 
@@ -37,25 +33,6 @@ def get_name_file(instance, filename):
     return 'portal/file'.join([get_random_string(5) + '_' + filename])
 
 
-class Order(models.Model):
-    name = models.CharField(max_length=200, verbose_name='Имя', blank=False)
-    summary = models.TextField(max_length=1000, help_text="Описание")
-    category = models.ForeignKey('Category', help_text="Выбор категории", on_delete=models.CASCADE)
-    photo_file = models.ImageField(max_length=200, upload_to=get_name_file, blank=True, null=True,
-                                   validators=[FileExtensionValidator(allowed_extensions=['png', 'jpg', 'jpeg'])])
-
-    def __str__(self):
-        return self.name
-
-    def delete(self, *args, **kwargs):
-        for ai in self.additionalimage_set.all():
-            ai.delete()
-        super().delete(*args, **kwargs)
-
-    def get_absolute_url(self):  # new
-        return reverse('order-create', args=[str(self.id)])
-
-
 class Category(models.Model):
     name = models.CharField(max_length=200, help_text="Укажите категорию")
 
@@ -63,17 +40,62 @@ class Category(models.Model):
         return self.name
 
 
-# ----------------------------------------------------------------------------------------------------------------------
-# Регистрация
-user_registrated = Signal()
+class Order(models.Model):
+    name = models.CharField(max_length=200, verbose_name='Имя', blank=False)
+    summary = models.TextField(max_length=1000, help_text="Описание")
+    category = models.ForeignKey('category', help_text="Выбор категории", on_delete=models.CASCADE)
+    photo_file = models.ImageField(max_length=200, upload_to=get_name_file, blank=True, null=True,
+                                   validators=[FileExtensionValidator(allowed_extensions=['png', 'jpg', 'jpeg'])])
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):  # new
+        return reverse('order-detail', args=[str(self.id)])
 
 
-# ----------------------------------------------------------------------------------------------------------------------
+class OrderInstance(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4,
+                          help_text="Уникальный идентификатор заказа")
+    order = models.ForeignKey('order', on_delete=models.SET_NULL, null=True)
+    summary = models.CharField(max_length=200)
+    due_back = models.DateField(null=True, blank=True)
+    customer_order = models.ForeignKey('user', on_delete=models.SET_NULL, null=True, blank=True)
+    LOAN_STATUS = (
+        ('n', 'Новая'),
+        ('a', 'Принято в работу'),
+        ('c', 'Выполнено'),
+    )
 
-class AdditionalImage(models.Model):
-    order_bb = models.ForeignKey(Order, on_delete=models.CASCADE, verbose_name='Объявление')
-    image = models.ImageField(upload_to=get_name_file, verbose_name='Изображение')
+    status = models.CharField(max_length=1, choices=LOAN_STATUS, blank=True, default='n', help_text='Статус заказа')
 
     class Meta:
-        verbose_name_plural = 'Дополнительные иллюстрации'
-        verbose_name = 'Дополнительная иллюстрация'
+        ordering = ["due_back"]
+        permissions = (("can_mark_returned", "Установите статус заказа"),)
+
+    def __str__(self):
+        """
+        Строка для представления объекта Model
+        """
+        return '%s (%s)' % (self.id, self.order.name)
+
+    @property
+    def is_overdue(self):
+        if self.due_back and date.today() > self.due_back:
+            return True
+        return False
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=200, verbose_name='Имя', blank=False)
+    surname = models.CharField(max_length=200, verbose_name='Фамилия', blank=False)
+    email = models.CharField(max_length=200, verbose_name='Почта', unique=True, blank=False)
+
+    def __str__(self):
+        return str(self.name) + ' ' + str(self.surname)
+
+    class Meta:
+        ordering = ['name']
+
+
+user_registrated = Signal()
--- portal/studio/urls.py
+++ portal/studio/urls.py
@@ -2,12 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('accounts/profile/order-create/<int:pk>', views.OrderCreate.as_view(), name='order-create'),
-    path('accounts/profile/order-create', views.OrderCreate.as_view(), name='order-create'),
-    path('accounts/register/done/', views.RegisterDoneView.as_view(), name='register_done'),
-    path('accounts/register/', views.RegisterUserView.as_view(), name='register'),
-    path('accounts/logout/', views.StudioLogoutView.as_view(), name='logout'),
-    path('accounts/profile/', views.profile, name='profile'),
-    path('accounts/login', views.StudioLoginView.as_view(), name='login'),
     path('', views.index, name='index'),
+    path('orders/', views.OrderListView.as_view(), name='orders'),
+    path('order/<int:pk>', views.OrderDetailView.as_view(), name='order-detail'),
+    path('myorders/', views.LoanedOrdersByUserListView.as_view(), name='my-order'),
+    path(r'customer/', views.LoanedOrdersAllListView.as_view(), name='all-order'),
+    path('order/create/', views.OrderCreate.as_view(), name='order-create'),
+    path('order/<int:pk>/update/', views.OrderUpdate.as_view(), name='order-update'),
 ]
